ae3adbf7f8aa165e4defb9210f3683582d48df73
Pek1ng <57554399+Pek1ng@users.noreply.github.com>
2022-12-21 22:11:26 +0800

    💕

--- Assets/Survival/Network/Server/Components/IdentityComponent.cs
+++ Assets/Survival/Network/Server/Components/IdentityComponent.cs
@@ -1,7 +1,9 @@
 using Unity.Entities;
+using Unity.NetCode;
 
 namespace Survival.Nework
 {
+    [GhostComponent(PrefabType = GhostPrefabType.Server)]
     public struct IdentityComponent : IComponentData
     {
 

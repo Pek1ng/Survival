00e5942c706a15baf5d0c18154054365d7504b99
Pek1ng <emptycity2578@aliyun.com>
2023-06-19 23:24:38 +0800

    添加主界面

--- Assets/Scripts/UI/Main/MainInterface.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Survival.UI
-{
-    public class MainInterface : MonoBehaviour
-    {
-       
-    }
-}
---
+++ Assets/Scripts/UI/Main/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Survival.UI
+{
+    public class MainInterface : MonoBehaviour
+    {
+        public TMP_InputField IPInputField;
+
+        public TMP_InputField PortInputField;
+
+        public void StartHost()
+        {
+            
+        }
+
+        public void StartServer()
+        {
+
+        }
+
+        public void StartClient()
+        {
+
+        }
+    }
+}
